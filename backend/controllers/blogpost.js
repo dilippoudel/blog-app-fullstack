b09295c12b7a641450fc77d510a325318a4dbcf8
Dilip <dilippoudel578@gmail.com>
2022-12-19 21:00:01 +0200

    Add exercise 4.1

---
+++ backend/controllers/blogpost.js
@@ -0,0 +1,63 @@
+const blogpostRouter = require('express').Router()
+const Blog = require('../models/blogpost')
+blogpostRouter.get('/', async (req, res, next) => {
+  try {
+    const allBlogs = await Blog.find({})
+    return res.json(allBlogs)
+  } catch (error) {
+    next(error)
+  }
+})
+blogpostRouter.get('/:id', async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+    return res.send(blog)
+  } catch (error) {
+    next(error)
+  }
+})
+blogpostRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await Blog.findByIdAndRemove(req.params.id)
+    return res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+blogpostRouter.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    if (body === undefined) {
+      return res.status(400).json({ error: 'content missing' })
+    }
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    })
+    await blog.save().then((savedBlog) => res.send(savedBlog))
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogpostRouter.put('/:id', async (req, res, next) => {
+  try {
+    const body = req.body
+    const newBlog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    }
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, newBlog, {
+      new: true,
+    })
+    res.json({ updatedBlog })
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = blogpostRouter
