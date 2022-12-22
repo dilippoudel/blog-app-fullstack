c08700bf94e0726d63f0f0e5d70246f925aba720
Dilip <dilippoudel578@gmail.com>
2022-12-22 20:06:05 +0200

    Added test case for total likes of blogposts

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,4 +2,10 @@
 const dummy = (blogs) => {
   return 1
 }
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  return blogs.map((blog) => blog.likes).reduce((a, c) => a + c, 0)
+}
+module.exports = { dummy, totalLikes }
