7721491fbed315aa77a6a4ced8839908e61681b4
Dilip <dilippoudel578@gmail.com>
2022-12-28 18:21:56 +0200

    Add basic validation for user and password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,6 +11,11 @@ usersRouter.get('/', async (req, res, next) => {
 })
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+  if (username.length < 3 || password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: 'username or password must be at least 3 chars long' })
+  }
   const existingUser = await User.findOne({ username })
   if (existingUser) {
     return response.status(400).json({ error: 'username must be unique' })
