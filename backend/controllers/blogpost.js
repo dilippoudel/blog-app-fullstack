692187f4e9dfdcc2747f78e752ca4cad8777a334
Dilip <dilippoudel578@gmail.com>
2023-01-02 18:44:29 +0200

    Implement middleware to blog delete route

--- backend/controllers/blogpost.js
+++ backend/controllers/blogpost.js
@@ -3,6 +3,8 @@ const { response } = require('express')
 const blogpostRouter = require('express').Router()
 const Blog = require('../models/blogpost')
 const User = require('../models/user')
+const middleware = require('../utils/middleware')
+
 blogpostRouter.get('/', async (req, res, next) => {
   try {
     const allBlogs = await Blog.find({}).populate('user', {
@@ -22,47 +24,57 @@ blogpostRouter.get('/:id', async (req, res, next) => {
     next(error)
   }
 })
-blogpostRouter.delete('/:id', async (req, res, next) => {
-  const blog = await Blog.findById(req.params.id)
-  const blog_owner_id = blog.user.toString()
-  const user = req.user.toString()
-  try {
-    if (user === blog_owner_id) {
-      await Blog.findByIdAndRemove(req.params.id)
-      return res.status(204).end()
+blogpostRouter.delete(
+  '/:id',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  async (req, res, next) => {
+    const blog = await Blog.findById(req.params.id)
+    const blog_owner_id = blog.user.toString()
+    const user = req.user.toString()
+    try {
+      if (user === blog_owner_id) {
+        await Blog.findByIdAndRemove(req.params.id)
+        return res.status(204).end()
+      }
+    } catch (error) {
+      next(error)
     }
-  } catch (error) {
-    next(error)
-  }
-})
-blogpostRouter.post('/', async (req, res, next) => {
-  const body = req.body
-  if (!req.user) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  if (body === undefined) {
-    return res.status(400).json({ error: 'content missing' })
-  }
-  if (body.title === undefined || body.url === undefined) {
-    return res.status(400).json({ error: 'title or url is mandatory' })
-  }
-  const user = await User.findById(body.userId)
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes ?? 0,
-    user: user._id,
-  })
-  try {
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    res.status(201).send(savedBlog)
-  } catch (error) {
-    next(error)
-  }
-})
+  },
+)
+blogpostRouter.post(
+  '/',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  async (req, res, next) => {
+    const body = req.body
+    if (!req.user) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    if (body === undefined) {
+      return res.status(400).json({ error: 'content missing' })
+    }
+    if (body.title === undefined || body.url === undefined) {
+      return res.status(400).json({ error: 'title or url is mandatory' })
+    }
+    const user = await User.findById(body.userId)
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ?? 0,
+      user: user._id,
+    })
+    try {
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      res.status(201).send(savedBlog)
+    } catch (error) {
+      next(error)
+    }
+  },
+)
 
 blogpostRouter.put('/:id', async (req, res, next) => {
   const body = req.body
